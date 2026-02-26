e96992355912c3f43a4ddf7b4d585d20ef74e434
salauddin <salauddin@hasan.com>
2026-02-26 07:06:16 +0600

    implement add, subtract, multiply using ES6 arrow functions

---
+++ arrow.js
@@ -0,0 +1,12 @@
+
+// ২. Arrow Function
+const add = (x, y) => x + y;
+console.log(add(5, 7));
+
+
+const sub = (p, q) => p - q ;
+console.log(sub(45, 30));
+
+
+const multy = (a, b) => a * b;
+console.log(multy(5, 4));
